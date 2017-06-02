5df0764f650f51b8b05e05c1ada09e02433caa53
Khushboo Bharti CW <kbharti.cw@mmm.com>
2017-06-02 14:56:30 +0530

    changed sidebar

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,41 +1,47 @@
-app.controller('loginController',
-    ['$scope', '$rootScope', '$location', 'AuthenticationService',
-    function ($scope, $rootScope, $location, AuthenticationService) {
-    	  console.log($location.path());
-        // reset login status
-        AuthenticationService.ClearCredentials();
 
-        $scope.login = function () {
-            $scope.dataLoading = true;
-            AuthenticationService.Login($scope.username, $scope.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials($scope.username, $scope.password);
-                    /*Need to discuss with Business for actual routing*/
-                  
-                    $location.path('/regbranch');
-                } else {
-                    $scope.error = response.message;
-                    $scope.dataLoading = false;
-                }
-            });
-        };
-    }]);
+
+app.controller('loginController',function ($scope, $rootScope, $location, $cookieStore, $http) {
+    // reset login status -- remove existing user
+    $cookieStore.remove('loggedInUser');
+    $scope.error = false;
+    $rootScope.showSideBar = false;
+    $scope.login = function () {
+        $scope.dataLoading = true;
+        //TODO Service call to do LDAP Authentication
+        var isAuthenticUser = true; // change value based on service call response
+
+        if(isAuthenticUser){
+            var loggedInUser = {
+                    userName : $scope.username
+            }
+            $cookieStore.put('loggedInUser', loggedInUser);
+
+            //TODO Need to discuss with Business for actual routing
+            $rootScope.showSideBar = true;
+            $location.path('/regbranch');
+            
+            
+        }else {
+            $scope.error = 'Invalid Username and/or Password !!!';
+            $scope.dataLoading = false;
+        }
+    };
+});
 
 app.run(['$rootScope', '$location', '$cookieStore', '$http',
     function ($rootScope, $location, $cookieStore, $http) {
-        // keep user logged in after page refresh
-        $rootScope.globals = $cookieStore.get('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; 
-        }
+    // keep user logged in after page refresh
 
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-        	 $rootScope.showSideBar = false;
-            // redirect to login page if not logged in
-            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-                $location.path('/login');
-               
-              
-            }
-        });
-    }]);
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        loggedInUser = $cookieStore.get('loggedInUser');         
+        $rootScope.showSideBar = true;
+        // redirect to login page if not logged in
+        if ($location.path() !== '/login' && !loggedInUser){
+            $location.path('/login');
+            $rootScope.showSideBar = false;
+            
+        }
+    });
+}]);
+
+
