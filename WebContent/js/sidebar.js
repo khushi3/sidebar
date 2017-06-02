329d07268d6e534646d76dd5177f83f311970c88
Khushboo Bharti CW <kbharti.cw@mmm.com>
2017-06-02 17:03:10 +0530

    fixed toggle bar

--- WebContent/js/sidebar.js
+++ WebContent/js/sidebar.js
@@ -1,4 +1,20 @@
-$("#menu-toggle").click(function(e) {
+
+
+app.directive('extLink', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elem) {
+      console.log(elem);
+      elem.bind('click', function(e) {
+          console.log("sidebar");
+        console.log(e);
         e.preventDefault();
-        $("#wrapper").toggleClass("active");
+        $("#wrapper").toggleClass("active")
+      });
+    }
+  };
+})
+
+app.controller('cnt', function($scope) {
+  
 });
